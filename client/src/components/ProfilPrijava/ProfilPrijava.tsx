a87b0cf2f0e72bbd11cce4315b54d7726057f6ea
Dusica Dosic <dosicdusica2@gmail.com>
2024-03-16 00:37:46 +0100

    prijava i meni

--- client/src/components/ProfilPrijava/ProfilPrijava.tsx
+++ client/src/components/ProfilPrijava/ProfilPrijava.tsx
@@ -1,9 +1,15 @@
 import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 import './ProfilPrijava.scss';
 
 const UserMenu = () => {
     const korisnikString = sessionStorage.getItem('korisnickoIme');
     const korisnik = korisnikString !== null ? JSON.parse(korisnikString) : null;
+    const [menuVisible, setMenuVisible] = useState(false);
+
+    const handleToggleMenu = () => {
+        setMenuVisible(!menuVisible);
+    };
   
     const handleLogout = () => {
       sessionStorage.removeItem('korisnickoIme');
@@ -12,11 +18,13 @@ const UserMenu = () => {
   
     return korisnik ? (
         <div className="user-menu">
-            <img className="imgKorisnik" src="Slike/korisnik.png" alt="korisnik" />
-            <ul id="meni">
-            <li>{korisnik}</li>
-            <li><Link to="/StranicaPrijava" onClick={handleLogout}>Odjava</Link></li>
-            </ul>
+            <img className="imgKorisnik" src="Slike/korisnik.png" alt="korisnik" onClick={handleToggleMenu}/>
+            {menuVisible && (
+                <ul id="meni">
+                <li>{korisnik}</li>
+                <li><Link to="/StranicaPrijava" onClick={handleLogout}>Odjava</Link></li>
+                </ul>
+            )}
         </div>
             ) : (
         <div className="user-menu">
